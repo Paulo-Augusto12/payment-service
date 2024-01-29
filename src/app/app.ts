c8dc2f7f4c8780462db6729ccd9a9077e0aecd41
Paulo Augusto Ribeiro <paulo-augusto12@outlook.com>
2024-01-29 14:24:23 -0300

    feat: creating structure for auth routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { userRouter } from "../router/userRotes/user.routes";
+import { authRouter } from "../router/authRoutes/auth.routes";
 
 const app = express();
 
 app.use("/users", userRouter);
+app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
   return res.status(200).json({ server: "Server is running" });
---
+++ src/router/authRoutes/auth.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+
+const authRouter = Router()
+
+authRouter.post('/login', (req, res) => {})
+
+authRouter.post('/signup', (req, res) => {})
+
+export { authRouter }
