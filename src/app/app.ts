75d89129a20bb6a0c3d1d48f759779f6d558bf4d
Paulo Augusto Ribeiro <paulo-augusto12@outlook.com>
2024-01-30 11:36:08 -0300

    feat: making all user routes private

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { authRouter } from "../router/authRoutes/auth.routes";
 import { userRouter } from "../router/userRotes/user.routes";
+import { validateJWT } from "../middlewares/validateJWT";
 
 const app = express();
 app.use(express.json());
 
-app.use("/users", userRouter);
+app.use("/users", validateJWT, userRouter);
 app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
