4996c15f427ea7031dcc84b6b28bfa823b457803
Paulo Augusto Ribeiro <paulo-augusto12@outlook.com>
2024-01-30 10:41:01 -0300

    feat: added jwt middleware

---
+++ src/middlewares/validateJWT.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+function validateJWT(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET as string, (error, userData) => {
+      if (error) {
+        return res
+          .status(500)
+          .json({ error: "Houove um erro por parte do servidor" });
+      }
+      next();
+    });
+  } else {
+    return res.status(401).json({
+      unauthorized:
+        "Um token de autenticação válido não foi providenciado durant sua requisição",
+    });
+  }
+}
+
+export { validateJWT };
